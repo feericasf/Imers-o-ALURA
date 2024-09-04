13aae313b723738457293c44ebed26717f6b5605
ferica <ericaffernanda@gmail.com>
2024-09-04 12:21:17 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Mercúrio",
+        descricao: "Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a uma distância média de apenas 57,9 milhões de quilômetros. Devido à sua proximidade com o Sol, Mercúrio experimenta temperaturas extremas, variando de -173°C à noite a 427°C durante o dia. Mercúrio não possui atmosfera significativa, o que contribui para suas grandes variações de temperatura.",
+        link: "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
+    },
+    {
+        titulo: "Vênus",
+        descricao: "Vênus, o segundo planeta mais próximo do Sol, é muitas vezes chamado de 'gêmeo da Terra' devido ao seu tamanho e composição semelhantes. No entanto, suas condições superficiais são radicalmente diferentes. Coberto por nuvens espessas de ácido sulfúrico, Vênus possui uma atmosfera extremamente densa que causa um efeito estufa descontrolado, elevando as temperaturas a níveis que derreteriam chumbo.",
+        link: "https://pt.wikipedia.org/wiki/V%C3%A9nus_(planeta)"
+    },
+    {
+        titulo: "Terra",
+        descricao: "A Terra é o terceiro planeta a partir do Sol e o único conhecido por abrigar vida. É um planeta rochoso com uma atmosfera rica em oxigênio, que permite a existência de uma grande variedade de formas de vida. A Terra possui um grande oceano de água líquida e um campo magnético que a protege da radiação solar.",
+        link: "https://pt.wikipedia.org/wiki/Terra"
+    }
+];
